641cb0806af927f4fb11dda0d6a64f7ee139cc5d
Ayanda-bolwatife <ayandastephen1000@gmail.com>
2023-04-08 23:06:11 +0100

    using styled components

--- src/RoomHome/About/About.jsx
+++ src/RoomHome/About/About.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import './About.css'
+import styled from 'styled-components'
 
 const About = ({about}) => {
     return (
+        <Wrapper>
             <section className="room-home-about">
                 <img src={about.imgDark} alt="" className="room-home-about-left-img"/>
                 <div className="room-home-about-text-content">
@@ -12,7 +13,88 @@ const About = ({about}) => {
                 <img  src={about.imgLight} alt=""
                 className="room-home-about-right-img"/>
             </section>
+        </Wrapper>
     )
 }
+const Wrapper = styled.section`
+    .room-home-about {
+  /* border: 5px solid blue; */
+  display: grid;
+  grid-template-columns: 28.82% auto 30.56%;
+  height: 100%;
+  /* overflow: hidden; */
+  width: 100%;
+}
 
+.room-home-about-left-img,
+.room-home-about-right-img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  max-height: 100%;
+  object-fit: cover;
+}
+
+.room-home-about-text-content {
+  padding: 0px 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.room-home-about-text-content > h5 {
+  margin-bottom: 20px;
+  font-size: 1.3em;
+  text-transform: uppercase;
+  letter-spacing: 4px;
+}
+.room-home-about-text-content > p {
+  font-size: 1.1em;
+  color: hsl(0, 0%, 63%);
+  line-height: 1.5;
+}
+
+@media only screen and (max-width: 1057px) {
+  .room-home-about {
+    grid-template-columns: auto;
+    grid-template-rows: 60% 40% 60%;
+  }
+
+  .room-home-about-text-content {
+    flex-direction: row;
+    padding: 0px 80px;
+    align-items: center;
+  }
+
+  .room-home-about-text-content > h5 {
+    margin: 0;
+    margin-right: 40px;
+    font-size: 2em;
+  }
+  .room-home-about-text-content > p {
+    font-size: 1em;
+    color: hsl(0, 0%, 63%);
+    line-height: 1.5;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .room-home-about {
+    grid-template-rows: 250px 1fr 250px;
+  }
+
+  .room-home-about-text-content {
+    padding: 40px 20px;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .room-home-about-text-content > h5 {
+    margin: 0;
+    margin-bottom: 20px;
+    font-size: 1.5em;
+  }
+}
+
+`
 export default About
--- src/RoomHome/RoomHome.jsx
+++ src/RoomHome/RoomHome.jsx
@@ -1,23 +1,59 @@
 import React from 'react'
-import './RoomHome.css'
 import {nav, sliderData as slider, aboutData as about} from './roomData.js'
 import Hero from './Hero/Hero'
 import About from './About/About'
 import Nav from './Nav/Nav'
+import styled from 'styled-components'
 
 const RoomHome = () => {
     return (
-        <div className='room-home' 
-        data-aos="fade-in"
-        data-aos-delay="150"
-        data-aos-duration="1000">
-            <Nav nav={nav}></Nav>
-            <main className="room-home-container">
-            <Hero slider={slider}></Hero>
-            <About about={about}></About>
+        <Wrapper>
+            <div className='room-home' 
+                data-aos="fade-in"
+                data-aos-delay="150"
+                data-aos-duration="1000">
+                <Nav nav={nav}></Nav>
+                <main className="room-home-container">
+                <Hero slider={slider}></Hero>
+                <About about={about}></About>
             </main>
         </div>
+        </Wrapper>
     )
 }
+const Wrapper = styled.section`
+    @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@500;600;700&display=swap');
 
+.room-home {
+  font-size: 12px;
+  font-family: 'League Spartan', sans-serif;
+  min-height: 100vh;
+  background-color: hsl(0, 0%, 100%);
+}
+
+/* MAIN  */
+
+.room-home-container {
+  display: grid;
+  height: 100vh;
+  min-height: 570px;
+  max-height: 100vh;
+  /* border: 5px solid green; */
+  grid-template-rows: 63% auto;
+}
+
+/* - Dark Gray: hsl(0, 0%, 63%)
+- Black: hsl(0, 0%, 0%)
+- White: hsl(0, 0%, 100%)
+- Very Dark Gray: hsl(0, 0%, 27%)  */
+
+@media only screen and (max-width: 1057px) {
+  .room-home-container {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+  }
+}
+
+`
 export default RoomHome;
